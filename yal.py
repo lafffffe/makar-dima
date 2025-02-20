bffc0c3e61cfa67e7021120e5eb119a421963890
lafffffe <dimagrebnev52@gmail.com>
2025-02-20 21:43:07 +0500

    yraa three commit

--- yal.py
+++ yal.py
@@ -2,12 +2,13 @@ import requests, pygame, sys, os
 
 
 server_address = 'https://static-maps.yandex.ru/v1?'
-api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
-ll = '37.530887,55.703118'
+api_key = '84e19037-c050-4c65-b7a7-4fb37ae52f28'
+x = 37.530887
+y = 55.703118
 spn = 0.002
 
-def response(server_address, api_key, ll, spn):
-    map_request = f"{server_address}ll={ll}&spn={str(spn)},{str(spn)}&apikey={api_key}"
+def response(server_address, api_key, x, y, spn):
+    map_request = f"{server_address}ll={str(x)},{str(y)}&spn={str(spn)},{str(spn)}&apikey={api_key}"
     response = requests.get(map_request)
 
     if not response:
@@ -21,21 +22,34 @@ def response(server_address, api_key, ll, spn):
         file.write(response.content)
     screen.blit(pygame.image.load(map_file), (0, 0))
     os.remove(map_file)
+    pygame.display.flip()
 
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-response(server_address, api_key, ll, spn)
+response(server_address, api_key, x, y, spn)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.K_PAGEUP:
-            spn += 0.01
-            response(server_address, api_key, ll, spn)
-        elif event.type == pygame.K_PAGEDOWN:
-            spn -= 0.01
-            response(server_address, api_key, ll, spn)
-    pygame.display.flip()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                spn += 0.001
+                response(server_address, api_key, x, y, spn)
+            elif event.key == pygame.K_PAGEDOWN:
+                spn -= 0.001
+                response(server_address, api_key, x, y, spn)
+            elif event.key == pygame.K_UP:
+                y += 0.00001
+                response(server_address, api_key, x, y, spn)
+            elif event.key == pygame.K_DOWN:
+                y -= 0.00001
+                response(server_address, api_key, x, y, spn)
+            elif event.key == pygame.K_RIGHT:
+                x += 0.00001
+                response(server_address, api_key, x, y, spn)
+            elif event.key == pygame.K_LEFT:
+                x -= 0.00001
+                response(server_address, api_key, x, y, spn)
 pygame.quit()
